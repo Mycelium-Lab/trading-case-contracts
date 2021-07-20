559c7767b9da70b1aedb8198ed97ab0b07124bd5
knoexus <knoexus@yahoo.com>
2021-07-20 17:52:48 +0300

    remove floating pragma caret

--- contracts/IPancakeSwapOracle.sol
+++ contracts/IPancakeSwapOracle.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.2;
+pragma solidity 0.6.2;
 
 interface IPancakeSwapOracle {
     function update() external returns (bool success);
--- contracts/TokenProxy.sol
+++ contracts/TokenProxy.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.2;
+pragma solidity 0.6.2;
 
 import "./ERC20.sol";
 import "@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol";
