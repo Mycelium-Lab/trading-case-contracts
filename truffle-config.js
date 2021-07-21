9dddd7894ee96cb86d124d66da731f34de4dd997
knoexus <knoexus@yahoo.com>
2021-07-21 12:59:55 +0300

    add kovan to config

--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,7 @@
  //
  // const fs = require('fs');
  const mnemonic = require('./secrets.json').mnenonic
+ const infuraId = require('./secrets.json').projectId
  
  module.exports = {
    /**
@@ -58,6 +59,14 @@
      // },
      // Useful for deploying to a public network.
      // NB: It's important to wrap the provider as a function.
+     kovan: {
+      provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${infuraId}`),
+      network_id: 42,
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 10,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+     },
      bscTestnet: {
        provider: () => new HDWalletProvider(mnemonic, 'https://data-seed-prebsc-1-s2.binance.org:8545/'),
        network_id: 97,       // Ropsten's id
